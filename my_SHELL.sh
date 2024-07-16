45b51e80977efead4e78ec85d8eb461aee652404
Ananth M <129041374+ananth-ms@users.noreply.github.com>
2024-07-16 00:31:46 -0700

    Update my_SHELL.sh

--- my_SHELL.sh
+++ my_SHELL.sh
@@ -5,8 +5,8 @@ sudo yum install git -y
 sudo git clone https://github.com/ananth-ms/jenkins_project.git
 ls
 cp -R jenkins_project/* .
-sudo build 590184013243.dkr.ecr.ap-south-1.amazonaws.com/jenkins_project
-sudo aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 590184013243.dkr.ecr.ap-south-1.amazonaws.com
+sudo build acco_no.dkr.ecr.ap-south-1.amazonaws.com/jenkins_project
+sudo aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin acco_no.dkr.ecr.ap-south-1.amazonaws.com
 ls
-sudo docker push 590184013243.dkr.ecr.ap-south-1.amazonaws.com/jenkins_project
-sudo docker run -d -p 80:80 590184013243.dkr.ecr.ap-south-1.amazonaws.com/jenkins_project
+sudo docker push acco_no.dkr.ecr.ap-south-1.amazonaws.com/jenkins_project
+sudo docker run -d -p 80:80 acco_no.dkr.ecr.ap-south-1.amazonaws.com/jenkins_project
